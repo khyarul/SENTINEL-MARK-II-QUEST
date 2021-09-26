68efd95d761461cb5ceb6ccbd6b00402d07acf28
Khyarul Arham <50608159+khyarul@users.noreply.github.com>
2021-09-26 20:30:31 +0700

    Delete sentinel_main directory

--- sentinel_main/sentinel_button.ino
+++
@@ -1,8 +0,0 @@
-void initButton() {
-  pinMode(PB13, INPUT_PULLUP);
-  pinMode(PB14, INPUT_PULLUP);
-  pinMode(PB15, INPUT_PULLUP);
-  pinMode(PC13, INPUT_PULLUP);
-  pinMode(PC14, INPUT_PULLUP);
-  pinMode(PC15, INPUT_PULLUP);
-}
